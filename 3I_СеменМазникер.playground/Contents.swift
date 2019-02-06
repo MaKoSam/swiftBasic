06e2875c95a403147a9f89b1ebb3deb4365c2b0d
MaKoSam <mazniker@icloud.com>
2019-02-06 15:50:17 +0500

    Улучшена структура машины: -Добавлен метод запуска двигателя; -Добавлен метод изменения состояния фар; -Добавлен метод изменения состояния окон; -Добавлен метод погрузки и выгрузки груза из багажника.
    Добавлены комментарии.

--- 3I_СеменМазникер.playground/Contents.swift
+++ 3I_СеменМазникер.playground/Contents.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  Lesson Three Home Work
+//  HW 3 (enum + struct)
 //
 //  Created by Sam Mazniker on 2/5/19.
 //  Copyright © 2019 Sam Mazniker. All rights reserved.
@@ -9,31 +9,143 @@
 import Foundation
 import UIKit
 
-struct vehicle  {
-    var brand : String
-    var produceDate : Int
-    var trunkCapacity : Int //объем багажника
-    var trunkCapacityFilled : Int
-    var isEngineOn : Bool
-    var isLightOn : Bool
+enum carEngine { //Состояние двигателя
+    case on
+    case off
+}
+
+enum carLight { //Состояние фар
+    case normal //Свет включен
+    case distance //Дальний свет включен
+    case off //Свет выключен
+}
+
+enum carWindows { //Состояние окон
+    case open
+    case close
+}
+
+enum inOutTrunk { //Для метода погрузки/выгрузки груза из багажника
+    case putIn //Загрузка
+    case putOut //Выгрузка
+}
+
+struct vehicle {
+    var brand : String //Марка авто
+    var color : String //Цвет
+    var produceDate : Int //Год производства
+    var trunkCapacity : Int //Объем багажника
+    var trunkCapacityFilled : Int //Объем заполненного груза в багажнике
     
-    init(brand : String, date : Int, trunk : Int){
-        self .brand = brand
-        produceDate = date
-        trunkCapacity = trunk
-        trunkCapacityFilled = 0
-        isEngineOn = false
-        isLightOn = false
+    //Состояние двигателя с уведомлением при изменении
+    var engineState : carEngine {
+        willSet {
+            if newValue == .on {
+                print("Двигатель запускается.")
+            } else {
+                print("Двигатель выключен.")
+            }
+        }
+    }
+    
+    //Состояние света фар с уведомлением при изменении
+    var lightState : carLight {
+        willSet {
+            if newValue == .normal {
+                print("Включен обычный свет фар.")
+            } else if newValue == .distance {
+                print("Включен дальний свет фар.")
+            } else {
+                print("Фары выключены.")
+            }
+        }
+    }
+    
+    //Состояние окон с уведомлением при изменении
+    var windowsState : carWindows {
+        willSet {
+            if newValue == .open {
+                print("Окна открываются.")
+            } else {
+                print("Окна закрываются.")
+            }
+        }
+    }
+    
+    //Упрощенная инициализация машины
+    init(_ brand : String, _ carColor: String, _ date : Int, trunk : Int){
+        self.brand = brand
+        self.color = carColor
+        self.produceDate = date
+        self.trunkCapacity = trunk
+        self.trunkCapacityFilled = 0
+        self.engineState = .off
+        self.lightState = .off
+        self.windowsState = .close
+    }
+    
+    //Метод: запуск и остановка двигателя
+    mutating func changeEngineState(){
+        self.engineState = self.engineState == .on ? .off : .on
+    }
+    
+    //Метод: открытие и закрытие окон
+    mutating func changeWindowsState(){
+        self.windowsState = self.windowsState == .open ? .close : .open
+    }
+    
+    //Метод: изменение состояния света фар
+    mutating func changeLightState(changeTo action : carLight){
+        self.lightState = action
+    }
+    
+    //Метод: погрузка и выгрузка груза определенного объема
+    mutating func trunkInOut(need mass: inOutTrunk, space: Int) -> String{
+        switch mass { //Определение действия: Выгрузка / Погрузка
+            
+        case .putIn: //Погрузка
+            if(space + trunkCapacityFilled) > trunkCapacity {
+                return "Недостаточно свободного места в багажнике"
+            } else {
+                trunkCapacityFilled += space
+                return "Груз, объемом \(space)л. погружен; Объем свободного места - \(trunkCapacity - trunkCapacityFilled)л."
+            }
+            
+        case .putOut: //Выгрузка
+            if space > trunkCapacityFilled {
+                return "В багажнике только \(trunkCapacityFilled)л. груза"
+            } else {
+                trunkCapacityFilled -= space
+                return "Груз, объемом \(space)л. выгружен; Объем свободного места - \(trunkCapacity - trunkCapacityFilled)л."
+            }
+        }
     }
     
     
 }
 
 
-var car : vehicle
-//car = (brand : "audi", date : 2014, trunk : 80)
-//todo: check for initializing of structure
-//todo: check for the enum (in working with the struct)
+
+var car = vehicle("audi", "white", 2014, trunk: 100)
+
+car.changeEngineState()
+car.changeEngineState()
+
+car.changeWindowsState()
+
+car.changeLightState(changeTo: .normal)
+car.changeLightState(changeTo: .distance)
+
+print(car.trunkInOut(need: .putIn, space: 50))
+print(car.trunkInOut(need: .putOut, space: 30))
+
+print(car)
+
+
+
+
+
+
 
 
 
