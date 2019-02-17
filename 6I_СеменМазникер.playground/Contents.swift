8fdbb2dbf9f90336ef410429a83718a960d7303d
MaKoSam <mazniker@icloud.com>
2019-02-17 15:37:18 +0500

    Выполнено первое задание: -Создана структура "очередь" (queue); -Инициализированы типовые функции очереди: Добавить в конец, Убрать в начале, Вывести размер.

--- 6I_СеменМазникер.playground/Contents.swift
+++ 6I_СеменМазникер.playground/Contents.swift
@@ -1,1 +1,31 @@
+//
+//  main.swift
+//  Lesson Six Home Work
+//
+//  Created by Sam Mazniker on 2/17/19.
+//  Copyright © 2019 Sam Mazniker. All rights reserved.
+//
+
+import Foundation
 import UIKit
+
+struct queue<T> { //Очередь
+    private var array : [T] = []
+    
+    mutating func pushBack (_ element : T){
+        array.append(element)
+    }
+    
+    mutating func popFirst () -> T? {
+        return array.remove(at: 0)
+    }
+    
+    func size() -> Int? {
+        return array.count
+    }
+}
+
+
+var a = queue<Int>() // queue a
+
+
