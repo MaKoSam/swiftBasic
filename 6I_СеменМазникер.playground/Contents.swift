e34f57eb2d19915188030e90bbf6c16f60f0e0bd
MaKoSam <mazniker@icloud.com>
2019-02-17 16:12:35 +0500

    Создан класс людей для реализации subscript. Реализован subscript.

--- 6I_СеменМазникер.playground/Contents.swift
+++ 6I_СеменМазникер.playground/Contents.swift
@@ -9,7 +9,24 @@
 import Foundation
 import UIKit
 
-struct queue<T> { //Очередь
+protocol Living {
+    var weight : Int { get set }
+}
+
+class Human : Living {
+    var name : String
+    var hight : Int
+    var weight : Int
+    
+    init(_ name : String, _ hight : Int, _ weight : Int){
+        self.name = name
+        self.hight = hight
+        self.weight = weight
+    }
+    
+}
+
+struct Queue<T : Living> {
     private var array : [T] = []
     
     mutating func pushBack (_ element : T){
@@ -20,12 +37,41 @@ struct queue<T> { //Очередь
         return array.remove(at: 0)
     }
     
-    func size() -> Int? {
+    func size() -> Int {
         return array.count
     }
+    
+    subscript(elements : Int ...) -> Int? {
+        var total : Int = 0
+        for index in elements {
+            if index > self.size() - 1 {
+                print("Fatal error. Index out of range.")
+                return nil
+            }
+            total += self.array[index].weight
+        }
+        return total
+    }
+    
 }
 
 
-var a = queue<Int>() // queue a
+let Alex = Human("Alex", 154, 50)
+let Bob = Human("Bob", 215, 114)
+let Ginger = Human("Gignger", 178, 59)
+let Marigold = Human("Marigold", 163, 54)
+
+var a = Queue<Human>()
+
+a.pushBack(Bob)
+a.pushBack(Marigold)
+a.pushBack(Alex)
+a.pushBack(Ginger)
+
+print(a[1, 2]) //subscript: Вывод суммы веса людей в очереди
+
+
+
+
 
 
